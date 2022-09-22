72d6cbf893c54697a801110bf564df1c5cc6f864
khadija-Amiri <75305568+khadija-Amiri@users.noreply.github.com>
2022-09-22 22:42:27 +0430

    Add files via upload

---
+++ web.php
@@ -0,0 +1,150 @@
+ 
+ 
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>new web</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body> 
+
+    <section class="first-part">
+
+    <h1> Univers.com</h1>
+        <a href="#sec">Services</a>
+        <a href="#abt">About </a>
+        <a href="#contact-part">Contact </a>
+      
+
+
+
+       <h1> welcome Dear users</h1>
+       <h2> Here you can find any info about the Galaxy</h2>
+       </section>  
+<section id="sec">
+         <h1> Services</h1>
+      <div class="univers">
+        <div class="univers1">
+         <a href=""> Earth </a>
+          <p> Earth is our planet Earth,our home planet,is a world unlike any other.The third planet from the sun,Earth is the only place in the known universe confirmed to host life.
+          Witha radius of 3,959 miles,Earth is the fifth largest planet in our solar system,and it's the only one known for sure to have liquid water on its surface. Earth is also unique in terms of monikers. Every other solar system planet was named for a Greek or Roman deity, but for at least a thousand years, some cultures have described our world using the Germanic word “earth,” which means simply “the ground.”</p>
+        
+        </div>
+      </div> 
+
+      <div class="univers">
+        <div class="univers2">
+          <a href=""> Mercury </a>
+          <p>Mercury is the smallest planet in our solar system. It’s just a little bigger than Earth’s moon. It is the closest planet to the sun, but it’s actually not the hottest. Venus is hotter.Mercury is one of the rocky planets. It has a solid surface that is covered with craters. It has no atmosphere, and it doesn’t have any moons. Mercury likes to keep things simple.</p>
+        </div>
+      </div> 
+
+      <div class="univers">
+        <div class="univers3">
+          <a href=""> Jupiter </a>
+          <p>Eleven Earths could fit across Jupiter’s equator. If Earth were the size of a grape, Jupiter would be the size of a basketball.Jupiter orbits about 484 million miles (778 million kilometers) or 5.2 Astronomical Units (AU) from our Sun (Earth is one AU from the Sun).Jupiter rotates once about every 10 hours (a Jovian day), but takes about 12 Earth years to complete one orbit of the Sun (a Jovian year).Jupiter is a gas giant and so lacks an Earth-like surface. 
+            If it has a solid inner core at all, it’s likely only about the size of Earth.</p>
+        </div>
+      </div>
+      
+      <div class="univers">
+        <div class="univers4">
+          <a href=""> Mars </a>
+          <p>Mars, the fourth planet from the sun, is famed for its rusty red appearance. The Red Planet is a cold, desert world with a very thin atmosphere. But the dusty, lifeless (as far as we know it) planet is far from dull. 
+
+            Phenomenal dust storms can grow so large they engulf the entire planet, temperatures can get so cold that carbon dioxide in the atmosphere condenses directly into snow or frost, and marsquakes — a Mars version of an earthquake — regularly shake things up. </p>
+        </div>
+      </div>
+
+      <div class="univers">
+        <div class="univers5">
+          <a href=""> Neptun </a>
+          <p>Dark, cold, and whipped by supersonic winds, ice giant Neptune is the eighth and most distant planet in our solar system.
+            The Latest
+             New Study Finds Unexpected Temperature Changes on Neptune
+             Hubble's Grand Tour of the Outer Solar System
+            More than 30 times as far from the Sun as Earth, Neptune is the only planet in our solar system not visible to the naked eye and the first predicted by mathematics before its discovery. In 2011 Neptune completed its first 165-year orbit since its discovery in 1846.</p>
+        </div>
+      </div>
+
+      <div class="univers">
+        <div class="univers6">
+          <a href=""> Pluto </a>
+          <p>Pluto is a dwarf planet in the Kuiper Belt, a donut-shaped region of icy bodies beyond the orbit of Neptune. There may be millions of these icy objects, collectively referred to as Kuiper Belt objects (KBOs) or trans-Neptunian objects (TNOs), in this distant region of our solar system.
+            Pluto – which is smaller than Earth’s Moon – has a heart-shaped glacier that’s the size of Texas and Oklahoma. This fascinating world has blue skies, spinning moons, mountains as high as the Rockies, and it snows – but the snow is red.</p>
+        </div>
+      </div>
+
+      <div class="univers">
+        <div class="univers7">
+          <a href=""> Saturn </a>
+          <p>Saturn is the sixth planet from the Sun and the second-largest planet in our solar system.The Latest
+             10 Things: Why Cassini Mattered Saturn and Titan Resources Adorned with thousands of beautiful ringlets, Saturn is unique among the planets. It is not the only planet to have rings – made of chunks of ice and rock – but none are as spectacular or as complicated as Saturn's.
+            
+            Like fellow gas giant Jupiter, Saturn is a massive ball made mostly of hydrogen and helium.</p>
+        </div>
+      </div>
+
+      <div class="univers">
+        <div class="univers8">
+          <a href=""> Uranus </a>
+          <p>Uranus is the seventh planet from the Sun, and has the third-largest diameter in our solar system. It was the first planet found with the aid of a telescope, Uranus was discovered in 1781 by astronomer William Herschel, although he originally thought it was either a comet or a star.</p>
+        </div>
+      </div>
+
+      <div class="univers">
+        <div class="univers9">
+          <a href=""> Venus </a>
+          <p>Venus is the second planet from the Sun and is Earth’s closest planetary neighbor. It’s one of the four inner, terrestrial (or rocky) planets, and it’s often called Earth’s twin because it’s similar in size and density. These are not identical twins, however – there are radical differences between the two worlds.</p>
+        </div>
+      </div>
+    </section>
+    <br>
+    <br> 
+    <div class="abt2"><h1>About us</h1></div>
+    <div id="abt" >
+    
+       <div><img  height="300" width="600"src="images/8.jpg"></div>
+       <div id="abt1"><p >      univers. com is one of the best resources for you, 
+        your support will grow us here you can search and find good informationabout our planet
+        univers. com is one of the best resources for you, 
+        your support will grow us here you can search and find good informationabout our planet
+        univers. com is one of the best resources for you, 
+        your support will grow us here you can search and find good informationabout our planet
+        univers. com is one of the best resources for you, 
+        your support will grow us here you can search and find good informationabout our planet univers. com is one of the best resources for you, 
+        your support will grow us here you can search and find good informationabout our planet univers. com is one of the best resources for you, 
+        your support will grow us here you can search and find good informationabout our planet.:)
+        </p>
+        </div>  
+</div>
+    
+    <div id="contact-part">
+
+      <h2> contact Us</h2>
+      <form action=" web.php" method="post">
+        <label >Name</label>
+         <input type="text" name="name"  >
+<br>
+<br>
+      <label > Email</label>
+       <input type="email" name="email-add"   >
+<br>
+<br>
+      <label > Comment</label>
+       <textarea name="" id="" cols="60" rows="3"></textarea>
+<br>
+<br>
+   <button type="submit">submit</button>
+      </form>
+</div>
+
+      <div class="footer"> <h1> Univers.com</h1></div>
+      </body>
+</html>
